28aca232864b9363363d763e587c1c7c08843968
Mehmet Aktaş <87446203+Drmehmetaktass@users.noreply.github.com>
2022-06-25 10:03:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,23 +24,19 @@ async def cancel(event):
 
 
 @client.on(events.NewMessage(pattern="^/start$"))
-async def start(event):
-  await event.reply("**LoungeTagger Bot**, Grup veya kanaldaki neredeyse tüm üyelerden bahsedebilirim ★\nDaha fazla bilgi için **/help**'i tıklayın.",
-                    buttons=(
-                      [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/loungetaggerbot?startgroup=a'),
-                      Button.url('📣 Support', 'https://t.me/loungesupport'),
-                      Button.url('🚀 Sahibim', 'https://t.me/bodrumlubebekk')]
-                    ),
-                    link_preview=False
-                   )
-@client.on(events.NewMessage(pattern="^/help$"))
-async def help(event):
-  helptext = "**Loungetagger bot'un Yardım Menüsü**\n\nKomut: /all \n  Bu komutu, başkalarına bahsetmek istediğiniz metinle birlikte kullanabilirsiniz. \n`Örnek: /all Günaydın!`  \nBu komutu yanıt olarak kullanabilirsiniz. herhangi bir mesaj Bot, yanıtlanan iletiye kullanıcıları etiketleyecek"
-  await event.reply(helptext,
-                    buttons=(
-                      [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/loungetaggerbot?startgroup=a'),
-                       Button.url('📣 Support', 'https://t.me/loungesupport'),
-                      Button.url('🚀 Sahibim', 'https://t.me/bodrumlubebekk')]
+async def info(event):
+  await event.reply("\n🌹HOŞGELDİNİZ🌹        \n⚔️𝙓𝙚𝙣𝙖⚔️\nAnka Etiket Botunu tercih ettiğiniz için teşekkür ederiz.\n**KOMUTLAR**\n/all herkesi etiketler\n/atag sadece admin\n/cancel botu durdurur\n\n𝚂𝙰𝙷İ𝙱İ𝙼:@Joaquin010\n\n𝙱𝙾𝚃 𝙶𝙴𝙻İŞ𝚃İ𝚁İ𝙲İ𝚂İ:@drmehmetaktass",
+                     buttons=(
+                      [
+                       Button.url('BENİ GRUBA EKLE➕', 'https://t.me/Aofanka_bot?startgroup=a')
+                      ],
+                      [
+                       Button.url('📢 KANAL', 'https://t.me/Ankasiyasetvekamu'),
+                       Button.url('🇹🇷 SAHİBİM', 'https://t.me/Joaquin010')
+                      ],
+                      [
+                       Button.url('🧑🏻‍💻 𝙳𝙰𝙷𝙰 𝙵𝙰𝚉𝙻𝙰 𝙱𝙾𝚃 🧑🏻‍💻', 'https://t.me/turkcbot')
+                      ],
                     ),
                     link_preview=False
                    )
